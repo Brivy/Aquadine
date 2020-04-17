7ac13548771483cf0463a17f017a82ca60c677d2
Brian van Yperen <b.yperen@aquadis.nl>
2020-04-17 23:32:36 +0200

    Added a db context

---
+++ Src/Infrastructure/Aquadine.Persistence/AquadineDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Aquadine.Domain.Entities;
+using Aquadine.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Aquadine.Persistence
+{
+    public class AquadineDbContext : DbContext, IAquadineDbContext
+    {
+        public AquadineDbContext()
+        {
+
+        }
+
+        public AquadineDbContext(DbContextOptions<AquadineDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Favorite> Favorites { get; set; }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public DbSet<NearestNeighbor> NearestNeighbors { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<PearsonCorrelation> PearsonCorrelations { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductIngredient> ProductIngredients { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Src/Infrastructure/Aquadine.Persistence/Interfaces/IAquadineDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Aquadine.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquadine.Persistence.Interfaces
+{
+    interface IAquadineDbContext
+    {
+        DbSet<Favorite> Favorites { get; set; }
+
+        DbSet<Ingredient> Ingredients { get; set; }
+
+        DbSet<Location> Locations { get; set; }
+
+        DbSet<NearestNeighbor> NearestNeighbors { get; set; }
+
+        DbSet<Order> Orders { get; set; }
+
+        DbSet<OrderDetail> OrderDetails { get; set; }
+
+        DbSet<PearsonCorrelation> PearsonCorrelations { get; set; }
+
+        DbSet<Product> Products { get; set; }
+
+        DbSet<ProductIngredient> ProductIngredients { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+        DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
