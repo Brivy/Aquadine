1febbce327dbd8efecefb623d6852888d4bf5409
Brian van Yperen <b.yperen@aquadis.nl>
2020-04-18 08:57:38 +0200

    Added a startup class for the persistence layer combined with an extension method that will automatically inject repositories

---
+++ Src/Infrastructure/Aquadine.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Aquadine.Persistence.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Automatically dependency inject repositories
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="repoNameSpace"></param>
+        /// <param name="interfaceNameSpace"></param>
+        public static void AddRepositories(this IServiceCollection services, string repoNameSpace, string interfaceNameSpace)
+        {
+            var repositories = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(x => string.Equals(x.Namespace, repoNameSpace, StringComparison.Ordinal));
+
+            foreach (var repo in repositories)
+            {
+                // Get the repository interface and throw an exception if more than one repository interface is found
+                var repoInterface = repo.GetInterfaces()
+                    .Where(x => string.Equals(x.Namespace, interfaceNameSpace, StringComparison.Ordinal))
+                    .Single();
+
+                services.AddScoped(repoInterface, repo);
+            }
+        }
+    }
+}
---
+++ Src/Infrastructure/Aquadine.Persistence/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Aquadine.Persistence.Extensions;
+using Aquadine.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aquadine.Persistence
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            const string repoNameSpace = "Aquadine.Persistence.Repositories";
+            const string interfaceNameSpace = "Aquadine.Persistence.Repositories.Interfaces";
+
+            services.AddRepositories(repoNameSpace, interfaceNameSpace);
+
+            services.AddDbContext<IAquadineDbContext, AquadineDbContext>((options) => 
+                options.UseSqlServer(configuration.GetConnectionString("AquadineDbConnection")));
+        }
+    }
+}
