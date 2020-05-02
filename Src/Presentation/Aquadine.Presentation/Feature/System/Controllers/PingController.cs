2f322dbfee82438a02f778e1f0778e5661353209
Brian van Yperen <b.yperen@aquadis.nl>
2020-05-02 12:30:16 +0200

    Added swagger annotatinos and xml comments to the ping controller

--- Src/Presentation/Aquadine.Presentation/Feature/System/Controllers/PingController.cs
+++ Src/Presentation/Aquadine.Presentation/Feature/System/Controllers/PingController.cs
@@ -1,7 +1,9 @@
 ﻿using Aquadine.Application.Core.Interfaces.Mediator;
 using Aquadine.Application.Features.System.Queries.Ping;
 using Aquadine.Presentation.Core.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,14 +16,19 @@ namespace Aquadine.Presentation.Feature.System.Controllers
 
         }
 
+        /// <summary>
+        /// Ping the application to check if everything is up and running
+        /// </summary>
         [HttpGet]
+        [SwaggerResponse(200, "API is up and running.")]
+        [SwaggerResponse(500, "The API has encountered an error.")]
         public async Task<IActionResult> Index(CancellationToken cancellationToken = default)
         {
             var result = await _querySender.SendQueryAsync(new PingSystemQuery(), cancellationToken);
 
             if (result == null)
             {
-                return BadRequest();
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
             return Ok(result);
