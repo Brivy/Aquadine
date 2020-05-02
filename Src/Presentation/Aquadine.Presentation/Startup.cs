c7477fd8421ee06c922adc9e1b78953795b47123
Brian van Yperen <b.yperen@aquadis.nl>
2020-05-02 12:29:56 +0200

    Added swagger configuration

--- Src/Presentation/Aquadine.Presentation/Startup.cs
+++ Src/Presentation/Aquadine.Presentation/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Reflection;
 
 namespace Aquadine.Presentation
 {
@@ -21,6 +24,18 @@ namespace Aquadine.Presentation
         {
             Connect.Startup.ConnectAquadineProjects(services, _configuration, typeof(Startup).Assembly);
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Aquadine API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(filePath);
+
+                c.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,8 +46,18 @@ namespace Aquadine.Presentation
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aquadine API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
